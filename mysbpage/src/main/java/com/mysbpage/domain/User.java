17d1278531ab809d58d82922cbd85ba244ffc2ec
김태목 <using1524@naver.com>
2019-01-08 21:12:24 +0900

    Spring Boot 반복주기 3-2
    1. 회원정보 수정 페이지 생성(updateForm.html)
    2. 회원정보 수정 기능 구현 ( 수정 화면으로 이동할 때 : @PathVariable, findBtId().get() & 수정을 실행할 때 : update() 메소드 작성, @PutMapping 방식으로 작동  )

--- mysbpage/src/main/java/com/mysbpage/domain/User.java
+++ mysbpage/src/main/java/com/mysbpage/domain/User.java
@@ -41,9 +41,16 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public void update(User updateUser) {
+		this.password = updateUser.password;
+		this.name = updateUser.name;
+		this.email = updateUser.email;
+	}
 	
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email : " + email + "]";
 	}
+	
+	
 }
--- mysbpage/src/main/java/com/mysbpage/web/UserController.java
+++ mysbpage/src/main/java/com/mysbpage/web/UserController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mysbpage.domain.User;
@@ -59,5 +61,18 @@ public class UserController {
 	@GetMapping("/login")
 	public String login() {
 		return "/user/login";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		model.addAttribute("user", userRepository.findById(id).get());
+		return "/user/updateForm";
+	}
+	@PutMapping("{id}")
+	public String update(@PathVariable Long id, User updateUser) {
+		User user = userRepository.findById(id).get();
+		user.update(updateUser);
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 }
