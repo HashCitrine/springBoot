191fa9397ad08c47a6ad8b0292fae81d716902c6
김태목 <using1524@naver.com>
2019-01-16 23:00:19 +0900

    Spring Boot 반복주기 5-1
    1. 질문 상세보기 페이지 구현 : show.html, model.addAttribute(questionRepository.findById(id).get())
    2. 질문 수정 기능 구현 : 회원정보 수정과 동일한 절차 (HttpSession, update메소드, questionRepository.save())
    3. 질문 삭제 기능 구현 : questionRepository.deleteById(id)

--- mysbpage/src/main/java/com/mysbpage/domain/Question.java
+++ mysbpage/src/main/java/com/mysbpage/domain/Question.java
@@ -1,8 +1,14 @@
 package com.mysbpage.domain;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Question {
@@ -10,17 +16,33 @@ public class Question {
 	@GeneratedValue
 	private Long id;
 	
-	private String writer;
+//	private String writer;
+	
+	@ManyToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
+	private User writer;		// 객체지향적인 관계를 형성하기 위해 -> 작성된 게시글의 작성자 정보 전체(User형 데이터)를 저장
+	
 	private String title;
 	private String contents;
+	private LocalDateTime createDate;
 	
 	public Question() {}
-	public Question(String writer, String title, String contents) {
+	public Question(User writer, String title, String contents) {
 		super();
 		this.writer = writer;
 		this.title = title;
 		this.contents = contents;
-		
+		this.createDate = LocalDateTime.now();
 	}
 	
+	public String getFormattedCreateDate() {
+		if(createDate == null) {
+			return "";
+		}
+		return createDate.format(DateTimeFormatter.ofPattern("yyyy.MM.DD HH:mm:ss"));
+	}
+	public void update(String updateTitle, String updateContents) {
+		this.title = updateTitle;
+		this.contents = updateContents;
+	}
 }
--- mysbpage/src/main/java/com/mysbpage/web/QuestionController.java
+++ mysbpage/src/main/java/com/mysbpage/web/QuestionController.java
@@ -4,8 +4,12 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mysbpage.domain.Question;
@@ -34,8 +38,45 @@ public class QuestionController {
 		}
 		
 		User sessionUser = HttpSessionUtils.getUserFromSession(session);
-		Question newQuestion = new Question(sessionUser.getUserId(), title, contents);
+		Question newQuestion = new Question(sessionUser, title, contents);
 		questionRepository.save(newQuestion);
 		return "redirect:/index";
+	}
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		model.addAttribute("show", questionRepository.findById(id).get());
+		return "/qna/show";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model, HttpSession session) {
+
+		model.addAttribute("show", questionRepository.findById(id).get());
+		return "/qna/update";
+	}
+	
+	@PutMapping("{id}")
+	public String update(@PathVariable Long id, String title, String contents, HttpSession session) {
+		Object tempUser = session.getAttribute(HttpSessionUtils.USER_SESSION_KEY);
+		if(tempUser == null) {
+			return "redirect:/users/login";
+		}
+		
+		User sessionedUser = (User)tempUser;
+		if(!sessionedUser.matchId(id)) {
+			throw new IllegalStateException("You can't access this page");
+		}
+		
+		Question updatedQuestion = questionRepository.findById(id).get();
+		updatedQuestion.update(title,contents);
+		questionRepository.save(updatedQuestion);
+		return String.format("redirect:/questions/%d", id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable Long id) {
+		questionRepository.deleteById(id);
+		return "redirect:/";
 	}
 }
